276bfb3252b0e18eefc46bf74d0c88b76c024fd1
unknown <s.bueschges@gmail.com>
2025-09-18 11:43:26 +0200

    Added Picker to Graphs and background color.

--- R/mod_responses_display.R
+++ R/mod_responses_display.R
@@ -27,6 +27,12 @@ mod_responses_display_ui <- function(id, tab_title) {
       bslib::nav_panel(
         title = "Graph",
         bslib::card_body(
+          shinyWidgets::pickerInput(
+            ns("workshop_set"),
+            label = "Workshop Set",
+            choices = c(),
+            multiple = TRUE
+          ),
           echarts4r::echarts4rOutput(outputId = ns("chart"))
         )
       )
@@ -56,6 +62,17 @@ mod_responses_display_server <- function(id, loading_function, binary_or_range) 
       responses(loading_function())
     })
     
+    observe({
+      req(responses()$workshop_set)
+      
+      shinyWidgets::updatePickerInput(
+        inputId = "workshop_set",
+        choices = responses()$workshop_set |> 
+          unique() |> 
+          sort()
+      )
+    })
+    
     output$responses_table <- DT::renderDataTable({
       DT::datatable(
         responses(),
@@ -77,11 +94,12 @@ mod_responses_display_server <- function(id, loading_function, binary_or_range) 
       
       if(binary_or_range == "binary") {
         
-        individual <- responses() %>% 
+        individual <- responses() |> 
+          dplyr::filter(workshop_set %in% input$workshop_set) |> 
           dplyr::mutate(Correct = ifelse(
             stringr::str_sub(response, start = 1L, end = 1L) == truth, TRUE, FALSE
-          )) %>% 
-          dplyr::group_by(user_session, round_number, workshop_set) %>% 
+          )) |>
+          dplyr::group_by(user_session, round_number, workshop_set) |>
           dplyr::summarise(
             Actual = sum(Correct), 
             Predicted = confidence |> stringr::str_replace("%", "") |> as.numeric() |> sum() / 100, 
@@ -89,8 +107,8 @@ mod_responses_display_server <- function(id, loading_function, binary_or_range) 
             .groups = "drop"
           )
         
-        group <- individual %>% 
-          dplyr::group_by(round_number) %>% 
+        group <- individual |>
+          dplyr::group_by(round_number) |>
           dplyr::summarise(
             Group_Pct_Actual = sum(Actual) / (dplyr::n() * mean(Total)), 
             Group_Pct_Predicted = sum(Predicted) / (dplyr::n() * mean(Total)), 
@@ -104,38 +122,39 @@ mod_responses_display_server <- function(id, loading_function, binary_or_range) 
         )
         
         # Create an interactive chart containing the group "binary" aggregated data
-        graph <- graph_list %>% 
-          purrr::pluck("group") %>% 
+        graph <- graph_list |>
+          purrr::pluck("group") |>
           dplyr::mutate(
             round_number = paste0("Round ", round_number)
-          ) %>% tidyr::drop_na() %>%  ### TODO // remove
-          echarts4r::e_charts(round_number) %>% 
-          echarts4r::e_bar(Group_Pct_Actual, name = "Actual % Correct") %>% 
+          ) |>tidyr::drop_na() |> ### TODO // remove
+          echarts4r::e_charts(round_number) |>
+          echarts4r::e_bar(Group_Pct_Actual, name = "Actual % Correct") |>
           echarts4r::e_line(
             Group_Pct_Predicted, 
             name = "Predicted % Correct", 
             symbol = "circle", 
             symbolSize = 20
-          ) %>% 
+          ) |>
           echarts4r::e_y_axis(
             formatter = echarts4r::e_axis_formatter(
               style = "percent", 
               digits = 0
             )
-          ) %>% 
+          ) |>
+          echarts4r::e_color(background = "white") |>
           echarts4r::e_tooltip(
             trigger = "axis", 
             formatter = echarts4r::e_tooltip_pointer_formatter(
               style = "percent", 
               digits = 1
             )
-          ) %>% 
+          ) |>
           echarts4r::e_toolbox_feature(feature = "saveAsImage")
         
       }
       
       if(binary_or_range == "range") {
-       
+        
         calculate_range_adjustment <- function(group_pct) {
           
           inv_norm <- qnorm(
@@ -148,12 +167,13 @@ mod_responses_display_server <- function(id, loading_function, binary_or_range) 
           
         }
         
-        individual <- responses() %>% 
-        dplyr::mutate(Bounded = dplyr::case_when(
-          truth >= lower_90 & truth <= upper_90 ~ TRUE, 
-          TRUE ~ FALSE
-        )) %>% 
-          dplyr::group_by(user_session, round_number, workshop_set) %>% 
+        individual <- responses() |> 
+          dplyr::filter(workshop_set %in% input$workshop_set) |> 
+          dplyr::mutate(Bounded = dplyr::case_when(
+            truth >= lower_90 & truth <= upper_90 ~ TRUE, 
+            TRUE ~ FALSE
+          )) |>
+          dplyr::group_by(user_session, round_number, workshop_set) |>
           dplyr::summarise(
             Bounded = sum(Bounded), 
             Total = dplyr::n(), 
@@ -161,8 +181,8 @@ mod_responses_display_server <- function(id, loading_function, binary_or_range) 
           )
         
         
-        group <- individual %>% 
-          dplyr::group_by(round_number) %>% 
+        group <- individual |>
+          dplyr::group_by(round_number) |>
           dplyr::summarise(
             Group_Pct = sum(Bounded) / (dplyr::n() * mean(Total)), 
             Adjustment_Needed = calculate_range_adjustment(group_pct = Group_Pct), 
@@ -175,33 +195,34 @@ mod_responses_display_server <- function(id, loading_function, binary_or_range) 
         )
         
         # Create an interactive chart containing the group "binary" aggregated data
-        graph <- graph_list %>% 
-          purrr::pluck("group") %>% 
+        graph <- graph_list |>
+          purrr::pluck("group") |>
           dplyr::mutate(
             Group = paste0("Round ", round_number), 
             Target = 0.90
-          ) %>% tidyr::drop_na() %>%  ### TODO // remove
-          echarts4r::e_charts(Group) %>% 
-          echarts4r::e_bar(Group_Pct, name = "Actual % Correct") %>% 
+          ) |>tidyr::drop_na() |> ### TODO // remove
+          echarts4r::e_charts(Group) |>
+          echarts4r::e_bar(Group_Pct, name = "Actual % Correct") |>
           echarts4r::e_line(
             Target, 
             name = "Target % Correct", 
             symbol = "circle", 
             symbolSize = 20
-          ) %>% 
+          ) |>
           echarts4r::e_y_axis(
             formatter = echarts4r::e_axis_formatter(
               style = "percent", 
               digits = 0
             )
-          ) %>% 
+          ) |>
+          echarts4r::e_color(background = "white") |>
           echarts4r::e_tooltip(
             trigger = "axis", 
             formatter = echarts4r::e_tooltip_pointer_formatter(
               style = "percent", 
               digits = 1
             )
-          ) %>% 
+          ) |>
           echarts4r::e_toolbox_feature(feature = "saveAsImage")
         
       }
